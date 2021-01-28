7b2a4197e0287510bd08e7c5a203628840f87a3a
Niko Bentley <nbently2021@MacBook-Pro-2.local>
2021-01-28 15:32:16 -0400

    added query and mutation for updating data and finding single record

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,6 @@
 const Mutations = {
   async createItem(parent, args, context, info) {
     // TODO: Check if they are logged in
-    console.log(Object.keys(context.db.item));
     const item = await context.db.item.create(
       {
         data: { ...args },
@@ -10,6 +9,18 @@ const Mutations = {
     );
 
     return item;
+  },
+  async updateItem(parent, args, context, info) {
+    // console.log(context.db.item);
+    // Make a copy of the updates
+    const updates = { ...args };
+    // Remove the ID from the update obj
+    delete updates.id;
+    // Run the update method
+    return context.db.item.update({
+      where: { id: parseFloat(args.id) },
+      data: updates,
+    });
   },
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,12 @@ const Query = {
     const items = context.db.item.findMany();
     return items;
   },
+  async item(parent, args, context, info) {
+    const item = context.db.item.findOne(args);
+    // console.log(args);
+
+    return item;
+  },
 };
 
 module.exports = Query;
