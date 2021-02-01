be8b3cad2792d19971ae17537aabd4836c667fd5
Niko Bentley <nbently2021@MacBook-Pro-2.local>
2021-02-01 16:20:57 -0400

    added cart styles and template on front end

---
+++ frontend/components/Cart.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import CartStyles from "./styles/CartStyles";
+import Supreme from "./styles/Supreme";
+import SickButton from "./styles/SickButton";
+import CloseButton from "./styles/CloseButton";
+
+export const LOCAL_STATE_QUERY = gql`
+  query LOCAL_STATE_QUERY {
+    cartOpen @client
+  }
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+  mutation TOGGLE_CART_MUTATION {
+    toggleCart @client
+  }
+`;
+
+export const Cart = () => {
+  return (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {(toggleCart) => (
+        <Query query={LOCAL_STATE_QUERY}>
+          {({ data }) =>
+            console.log(data) || (
+              <CartStyles open={data.cartOpen}>
+                <header>
+                  <CloseButton title="close" onClick={toggleCart}>
+                    &times;
+                  </CloseButton>
+                  <Supreme>Your Cart</Supreme>
+                  <p>You have __ Items in your cart.</p>
+                </header>
+
+                <footer>
+                  <p>$10.10</p>
+                  <SickButton>Checkout</SickButton>
+                </footer>
+              </CartStyles>
+            )
+          }
+        </Query>
+      )}
+    </Mutation>
+  );
+};
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Router from "next/router";
 import NProgress from "nprogress";
 import { Nav } from "./Nav";
+import { Cart } from "./Cart";
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -64,6 +65,6 @@ export const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
